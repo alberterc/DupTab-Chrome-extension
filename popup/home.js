a07cdec9c2b7288663839f938bdcba8cda59b16d
alberterc <85069947+alberterc@users.noreply.github.com>
2023-10-23 02:18:40 +0700

    Changed all of popup's functions
    - Remove functions which shows all opened tabs
    - Add new functions to highlight and group duplicate current active tab

--- popup/home.js
+++ popup/home.js
@@ -1,39 +1,97 @@
-function getAllTabsList() {
-    chrome.tabs.query({ currentWindow: true }, (tabs) => {
-        const tabListTitle = document.getElementById('tabListContainer');
-        const dupTabListTitle = document.getElementById('duplicateTabListContainer');
-        let urlTabs = getDuplicateTabsUrls(tabs).map((url) => {
-            return { url: url }
-        });
-        renderTabsList(tabs, tabListTitle);
-        renderTabsList(urlTabs, dupTabListTitle);
+var browser = browser || chrome;
+
+document.addEventListener('DOMContentLoaded', function() {
+    let currTab = [];
+    let allTab = [];
+    getCurrentTab().then(function(tab) {
+        currTab = tab;
     });
+    getAllTab().then(function(tabs) {
+        allTab = tabs;
+    });
+
+    const $highlightAllDupTabBtn = document.getElementById('highlightAllDupTabBtn');
+    const $groupAllDupTabBtn = document.getElementById('groupAllDupTabBtn');
+    
+    $highlightAllDupTabBtn.addEventListener('click', function() {
+        let duplicateTabIdsWithCurr = getDuplicateTabIdsWithCurr(currTab, allTab);
+        highlightAllDupTab(duplicateTabIdsWithCurr);
+    });
+    $groupAllDupTabBtn.addEventListener('click', function() {
+        let duplicateTabIdsWithCurr = getDuplicateTabIdsWithCurr(currTab, allTab);
+        groupAllDupTabBtn(duplicateTabIdsWithCurr);
+    });
+})
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function highlightAllDupTab(duplicateTabIdsWithCurr) {
+    let duplicateTabIdsWithCurrLength = duplicateTabIdsWithCurr.length;
+    for (let i = 0; i < duplicateTabIdsWithCurrLength; i++) {
+        browser.tabs.update(
+            duplicateTabIdsWithCurr[i],
+            {
+                active: false,
+                highlighted: true
+            }
+        )
+    }
+}
+
+function groupAllDupTabBtn(duplicateTabIdsWithCurr) {
+    if (duplicateTabIdsWithCurr.length > 0) {
+        browser.tabs.group(
+            {
+                tabIds: duplicateTabIdsWithCurr
+            }
+        )
+    }
+}
+
+function getDuplicateTabIdsWithCurr(currTab, allTab) {
+    let duplicateUrls = getDuplicateTabsUrls(allTab);
+    let duplicateTabIds = getDuplicateTabIds(allTab, duplicateUrls);
+    let duplicateTabIdsLength = duplicateTabIds.length;
+    let duplicateTabIdsWithCurr = [];
+    for (let i = 0; i < duplicateTabIdsLength; i++) {
+        if (duplicateUrls.indexOf(currTab[0].url) > -1) {
+            duplicateTabIdsWithCurr.push(duplicateTabIds[i]);
+        }
+    }
+    return duplicateTabIdsWithCurr;
 }
 
 function getDuplicateTabsUrls(tabs) {
-    const tabsUrlCount = tabs.map((tab) => {
+    let tabsUrlCount = tabs.map((tab) => {
         return { count: 1, tab: tab };
     }).reduce((result, b) => {
         result[b.tab.url] = (result[b.tab.url] || 0) + b.count;
         return result;
-    }, {});
-    const duplicateUrls = Object.keys(tabsUrlCount).filter((a) => tabsUrlCount[a] > 1);
-
+    }, {}); 
+    let duplicateUrls = Object.keys(tabsUrlCount).filter((a) => tabsUrlCount[a] > 1);
     return duplicateUrls;
 }
 
-function renderTabsList(tabs, element) {
-    const ul = document.createElement('ul');
+function getDuplicateTabIds(tabs, duplicateUrls) {
     let tabsLength = tabs.length;
+    let duplicateUrlsLength = duplicateUrls.length;
+    let duplicateTabIds = [];
     for (let i = 0; i < tabsLength; i++) {
-        const li = document.createElement('li');
-        li.innerHTML = tabs[i].url;
-        ul.appendChild(li);
+        for (let j = 0; j < duplicateUrlsLength; j++) {
+            if (tabs[i].url === duplicateUrls[j]) {
+                duplicateTabIds.push(tabs[i].id);
+            }
+        }
     }
-    element.appendChild(ul);
+    return duplicateTabIds;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const refreshListBtn = document.getElementById('refreshTabListBtn');
-    refreshListBtn.addEventListener('click', getAllTabsList);
-})
+function getCurrentTab() {
+    return browser.tabs.query({currentWindow: true, active: true});
+}
+
+function getAllTab() {
+    return browser.tabs.query({currentWindow: true});
+}
